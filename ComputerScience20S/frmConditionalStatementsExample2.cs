7097640edb95af0ed3e7b21f71ec7978460643b3
Mr. Wachs <mrLWachs@gmail.com>
2021-05-21 13:46:22 -0500

    Update frmConditionalStatementsExample2.cs

--- ComputerScience20S/frmConditionalStatementsExample2.cs
+++ ComputerScience20S/frmConditionalStatementsExample2.cs
@@ -29,47 +29,54 @@ namespace ComputerScience20S
 
         private void frmExample2_Load(object sender, EventArgs e)
         {
-            // This is the code that runs before the form appears, 
-            // you get to this code by double clicking the form
+            // This is the code that runs before the form 
+            // appears, you get to it by double clicking the form
+
+            // Welcome the user
             
-            // Welcome the user...
             MessageBox.Show("Hi, time to guess...");
-        
-            // The "focus" means it "goes" to that thing
-            txtFirst.Focus();
+
+            // Pick 2 random numbers and store them in our globals
+            // by first creating a random number generator
             
-            // pick 2 random #s
+            Random random = new Random();
 
-            // creates a random number generator
-            Random random = new Random();           
+            // Now use it (remember the second number is always one more)
 
-            //now use it
             number1 = random.Next(1, 6);
             number2 = random.Next(1, 4);
-            // remember the second number is always one more            
+
+            // Set the "focus" (means the program "goes" to that thing)
+            
+            txtFirst.Focus();          
         }
 
         private void btnGuess_Click(object sender, EventArgs e)
         {
-
-            // get the user guesses from the textboxes
-
-            // do two things at once, get the textbox text and convert at once
+            // Retrieve the user guesses from the textboxes...
+            // This time we will do two things at once, get the textbox 
+            // text and convert the text into a number all in one line
+            
             int guess1 = Convert.ToInt32(txtFirst.Text);
             int guess2 = Convert.ToInt32(txtSecond.Text);
 
-            // variables to remember if we guesses each:
+            // Create True/False boolean variables to remember if we 
+            // guess each (these are also known as "flags")
+
             bool gotFirst = false;
             bool gotSecond = false;
 
-            // now check the number vs. the guess
+            // Now we check the first number vs. the first guess...
+
             if (guess1 == number1)
             {
+                // User guessed it, flag it and tell user                
                 gotFirst = true;
                 lblFirst.Text = "Correct!";
             }
             else if (guess1 < number1)
             {
+                // Give the user a hint
                 lblFirst.Text = "Too low";
             }
             else if (guess1 > number1)
@@ -77,7 +84,15 @@ namespace ComputerScience20S
                 lblFirst.Text = "Too high";
             }
 
-            // now second number (use copy/paste and find/replace) speeds up coding
+            // The "else if" makes the code decide multiple "conditions"
+            // or multiple "decisions". You can add as many "else if" (each
+            // with its own 'test') as you want. You also have the option to
+            // add a "else" at the end (but that is not necessary)
+
+            // Now the same logic all over again, but with the second number
+            // and the second text box. TIP: use the copy/paste (CTRL + C 
+            // and CTRL + V) and find/replace (CTRL + H) to speed up coding
+
             if (guess2 == number2)
             {
                 gotSecond = true;
@@ -92,22 +107,34 @@ namespace ComputerScience20S
                 lblSecond.Text = "Too high";
             }
 
-            // check to see if we are done
+            // Check to see if we are done, using combined logic...
+
             if (gotFirst == true && gotSecond == true)
             {
-                // && means "and"
+                // The symbols "&&" means "AND" logically 
+                // Meaning both things have to be true for the entire
+                // test to be true
+
                 MessageBox.Show("You win!");
             }
             else if (gotFirst == true || gotSecond == true)
             {
-                // || means "or"
+                //  The symbols "||" means "OR" logically
+                // Meaning either of the two things can be true for the 
+                // entire test to be true
+
                 MessageBox.Show("You got one!");
+
+                // Count this as a guess
                 guesses = guesses + 1;
             }
             else
             {
+                // Count this as a guess
                 guesses = guesses + 1;
             }
+
+            // Display the guesses to the user...
 
             lblNumber.Text = "Number of Guesses = " + guesses;
         }
