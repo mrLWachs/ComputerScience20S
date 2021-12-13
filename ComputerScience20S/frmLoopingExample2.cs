e70a4016c5b736573c6aa1e8c10acc924657788f
Mr. Wachs <mrLWachs@gmail.com>
2021-12-13 15:32:56 -0600

    Update frmLoopingExample2.cs

--- ComputerScience20S/frmLoopingExample2.cs
+++ ComputerScience20S/frmLoopingExample2.cs
@@ -13,12 +13,13 @@ namespace ComputerScience20S
     public partial class frmLoopingExample2 : Form
     {
 
-        // Global Variables/Constants:
-
-        const int SPACER = 2;
-
+        // Global variables/constants:
         int width = 0;
         int height = 0;
+
+        // This acts as a 'gap' between the circles we will draw
+        // and any other things we are moving around
+        const int SPACER = 5;
 
 
         public frmLoopingExample2()
@@ -28,81 +29,97 @@ namespace ComputerScience20S
 
         private void btnExit_Click(object sender, EventArgs e)
         {
+            // Another way to close (exit) one form
             this.Close();
         }
 
         private void frmExample_Load(object sender, EventArgs e)
-        {
-            // set the form's background color to black
+        {        
+            // Set the form's background color
             this.BackColor = Color.Black;
 
-            // set the form to have no border and maximized (full screen)
+            // Set the form to have no border
             this.FormBorderStyle = FormBorderStyle.None;
+
+            // Set the form to be maximized (full screen)
             this.WindowState = FormWindowState.Maximized;
 
-            // get and remember the height and width of the form (screen)
-            width = frmLoopingExample2.ActiveForm.Width;
-            height = frmLoopingExample2.ActiveForm.Height;
+            // Get (and remember in our global variables) the width and 
+            // height of the screen (the form now fills the screen) - the
+            // keyword "this" refers to "this form"
+            width = this.Width;
+            height = this.Height;
 
-            // position the exit button based on this new size
-            btnExit.Top = SPACER;
+            // Set the button over to the right of the screen width
+            // based on the values we just got in our global variables
             btnExit.Left = width - btnExit.Width - SPACER;
+            btnExit.Top = 0 + SPACER;
 
-            // position the run button based on the exit button
-            btnRun.Top = btnExit.Top + btnExit.Height + SPACER;
+            // "Left" is the same as "X" coordinate
+            // "Top" is the same as "Y" coordinate
+
+            // Move the run button to match up with the exit button
             btnRun.Left = width - btnRun.Width - SPACER;
+            btnRun.Top = btnExit.Top + btnExit.Height + SPACER;
 
-            // position the up/down number based on the run button
-            nudSize.Top = btnRun.Top + btnRun.Height + SPACER;
+            // Move the up down size selector to match up with the run button
             nudSize.Left = width - nudSize.Width - SPACER;
+            nudSize.Top = btnRun.Top + btnRun.Height + SPACER;
 
-            // set the min and max of the circles to draw based on size
-            nudSize.Maximum = height - SPACER * 2;
+            // Set the minimum and maximum of the circles radius we can draw
             nudSize.Minimum = SPACER;
+            nudSize.Maximum = height - (SPACER * 2);            
         }
 
         private void btnRun_Click(object sender, EventArgs e)
         {
-            // show the color chooser dialog box
+            // Show the user a color choosing dialog box
             cdbColor.ShowDialog();
 
-            // create a color from the chooser
+            // Create a "color variable" (remembers a color) for the chooser
             Color color = cdbColor.Color;
 
-            // create a surface to draw on
+            // Create a surface to draw on (we are working with other 
+            // people's code to do a lot of the graphics drawing)
             Graphics surface = this.CreateGraphics();
 
-            // clear that surface for a new drawing
+            // "this" again means "this form"
+
+            // Clear (wipe) the surface to a color to draw on
             surface.Clear(Color.Black);
 
-            // create a "pen" to draw the outside of a circle
+            // Create a "pen" to draw the outside (line) of a circle
             Pen pen = new Pen(Color.White, 1);
 
-            // create a "brush" to fill in the circle with color
+            // Create a "brush" to fill in the circle with color
             Brush brush = new SolidBrush(color);
-            
-            // make variable for size of the circles
+
+            // Make a variable for the size of the circles
             int size = (int)nudSize.Value;
 
-            // make variable for how far apart the circles are
+            // Make a variable for how far apart the circles are
             int move = size + SPACER;
 
-            // make variable for the max number of circles we can make going down
+            // Make a variable for the max number of circles we can 
+            // draw going down ("Y" axis)
             int maxY = height - size - SPACER;
 
-            // make variable for the max number of circles we can make going across
+            // Make a variable for the max number of circles we can 
+            // draw going across ("X" axis)
             int maxX = width - size - SPACER;
 
-            // use for loops (nested) to keep drawing circles
+            // Use the "for" loop to repeatitively draw circles
             for (int y = SPACER; y <= maxY; y = y + move)
             {
+                // "Nest" a loop inside our loop
                 for (int x = SPACER; x <= maxX; x = x + move)
                 {
-                    // now draw the outside line of the circle
+                    // Now draw the inside of the circle (fill)
                     surface.FillEllipse(brush, x, y, size, size);
 
-                    // draw the inside color for the circle
+                    // Now draw the outside (line) of the circle
                     surface.DrawEllipse(pen, x, y, size, size);
+
                 }
             }
         }
