0d6de03dafd53a811fd7d6d29713feb7b92923aa
Mr. Wachs <mrLWachs@gmail.com>
2022-01-10 14:48:16 -0600

    Update frmProgrammingExtras1Form1.cs

--- ComputerScience20S/frmProgrammingExtras1Form1.cs
+++ ComputerScience20S/frmProgrammingExtras1Form1.cs
@@ -22,19 +22,55 @@ namespace ComputerScience20S
 
         private void tmrColors_Tick(object sender, EventArgs e)
         {
+            
+            // The timer runs like a loop, running this code
+            // over and over again - while the Enabled property
+            // is set to true. It runs based on time in milliseconds
+            // 1 second of real time = 1000 milliseconds
+            // 2 seconds of real time = 2000 milliseconds
+            // 10 seconds of real time = 10,000 milliseconds
+            // 1/2 second of real time = 500 milliseconds
+
+            // Create a random number generator
+            // and create random colors
+
             Random random = new Random();
+
+            // create three variables for the 3 colors
             int red   = random.Next(0, 256);
             int green = random.Next(0, 256);
             int blue  = random.Next(0, 256);
+
+            // Assign these random values to the background
+            // color of our current form
             this.BackColor = Color.FromArgb(red, green, blue);
+            
         }
 
         private void tmrChange_Tick(object sender, EventArgs e)
         {
-            tmrChange.Enabled = false;
-            frmProgrammingExtras1Form2 form2 = new frmProgrammingExtras1Form2();
-            form2.Show();
+            // This timer waits 3 seconds and then
+            // runs this code...
+
+            // Shut off the other timer
+            tmrColors.Enabled = false;
+
+            // Hide our current form (make it "disappear")
             this.Hide();
+
+            // Shift over to our second form
+            // to make another form, go to the menu:
+            // Project -> Add Form
+
+            // Creates a second form
+            Form2 form2 = new Form2();
+
+            // show that form
+            form2.Show();
+
+            // turn off this timer
+            tmrChange.Enabled = false;
+            
         }
 
         public static void endExample()
