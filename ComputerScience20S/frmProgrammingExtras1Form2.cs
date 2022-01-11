85d93116d1554b14dae7b2f44488f0b086e9e644
Mr. Wachs <mrLWachs@gmail.com>
2022-01-11 14:52:27 -0600

    Update frmProgrammingExtras1Form2.cs

--- ComputerScience20S/frmProgrammingExtras1Form2.cs
+++ ComputerScience20S/frmProgrammingExtras1Form2.cs
@@ -19,14 +19,35 @@ namespace ComputerScience20S
 
         private void button1_Click(object sender, EventArgs e)
         {
+            // Code to play the sound file
+
+            // How do files work?
+
+            // File have 3 names...
+            // first name (for example "C:\Users\lawrence.wachs\Desktop") - or path, location
+            // middle name (for example "sound")
+            // last name (for example ".wav") - or extension
+
+            // Use the open file dialog to choose a file
+            // it will give us all three names
+
+            // fun option, only filter in 
+            // files that are "wav" files
             openFileDialog1.Filter = "Sound Files|*.wav";
+
+            // Makes the dialog appear and choose a file
             openFileDialog1.ShowDialog();
+
+            // Load the sound file into the player
             axWindowsMediaPlayer1.URL = openFileDialog1.FileName;
+
+            // Play the sound
             axWindowsMediaPlayer1.Ctlcontrols.play();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+            // Use the same code and change the filtering..
             openFileDialog1.Filter = "Video Files|*.mp4";
             openFileDialog1.ShowDialog();
             axWindowsMediaPlayer1.URL = openFileDialog1.FileName;
@@ -45,7 +66,9 @@ namespace ComputerScience20S
 
         private void button5_Click(object sender, EventArgs e)
         {
+            // Hide the controls on the media player
             axWindowsMediaPlayer1.uiMode = "full";
+            // "ui" is short for "user interface"
         }
 
         private void button6_Click(object sender, EventArgs e)
@@ -55,9 +78,16 @@ namespace ComputerScience20S
 
         private void button7_Click(object sender, EventArgs e)
         {
-            frmProgrammingExtras1Form3 form3 = new frmProgrammingExtras1Form3();
-            form3.Show();
+            // Also code like..
             this.Close();
+
+            // the code above only closes the current form
+            // means the rest of the program (other forms)
+            // could still be running...
+
+            // switch to my last form
+            Form3 form3 = new Form3();
+            form3.Show();
         }
         
     }
