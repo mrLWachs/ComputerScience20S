4ae488a7297b4c29add93b4c7676a527bdf4afb0
Mr. Wachs <mrLWachs@gmail.com>
2022-01-12 14:51:38 -0600

    Update frmProgrammingExtras2.cs

--- ComputerScience20S/frmProgrammingExtras2.cs
+++ ComputerScience20S/frmProgrammingExtras2.cs
@@ -13,13 +13,15 @@ namespace ComputerScience20S
     public partial class frmProgrammingExtras2 : Form
     {
 
-        // Global variables/constants:
+        // Area for global variables:
 
-        const int KEY_END   = 27;
-        const int KEY_UP    = 38; // "or" W - 87
-        const int KEY_DOWN  = 40; // "or" S - 83
-        const int KEY_LEFT  = 37; // "or" A - 65
-        const int KEY_RIGHT = 39; // "or" D - 68
+        // constants...
+
+        const int KEY_END   = 27;     // Escape key
+        const int KEY_UP    = 38;     // Up key (or "W" would be 87)
+        const int KEY_DOWN  = 40;     // Down key (or "S" would be 83)
+        const int KEY_LEFT  = 37;     // Left key (or "A" would be 65)
+        const int KEY_RIGHT = 39;     // Right key (or "D" would be 68)
 
         const int MOVE_UP    = 1;
         const int MOVE_DOWN  = 2;
@@ -69,6 +71,12 @@ namespace ComputerScience20S
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
+            // By going to the form, then the properties window, then
+            // clicking on the "lightning bolt" at the top of the 
+            // properties window, scroll down to "KeyDown" and double
+            // click to get to this code... this code runs when the user
+            // presses a key
+            
             if      (e.KeyValue == KEY_END)   this.Close();
             else if (e.KeyValue == KEY_DOWN)  pacmanDirection = MOVE_DOWN;
             else if (e.KeyValue == KEY_UP)    pacmanDirection = MOVE_UP;
@@ -79,6 +87,25 @@ namespace ComputerScience20S
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+        
+            // The timer that runs the game
+
+            // (1) Get the coordinates of all four things (objects) - which
+            //     are: Pacman, Wall 1, Dot 1, and Ghost 1 (where are they 
+            //     at this moment in time)
+
+            // (2) Move some of the coordinates (Pacman, potentially the
+            //     ghost) - based on the direction the user set when the
+            //     user preseed down a key (e.g. we move the pacman 
+            //     coordinates up when the user presses the up key)
+
+            // (3) Collision detection - is pacman "touching" with:
+            //     dot (collsion reaction - dot disappears), ghost
+            //     (game over), wall (stop pacman at wall)
+
+            // (4) Redraw pacman on the form in the new location
+            //     from coordinates
+        
             getTheCoordinates();
             moveTheCoordinates();
             checkForCollisions();
