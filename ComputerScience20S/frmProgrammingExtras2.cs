40c92bd3ca9893a0a29f570cb5764b000a4ed9fe
Mr. Wachs <mrLWachs@gmail.com>
2022-01-13 14:50:14 -0600

    Update frmProgrammingExtras2.cs

--- ComputerScience20S/frmProgrammingExtras2.cs
+++ ComputerScience20S/frmProgrammingExtras2.cs
@@ -29,9 +29,14 @@ namespace ComputerScience20S
         const int MOVE_RIGHT = 4;
         const int STOPPED    = 0;
 
+        const int MOVE_AMOUNT = 2;
+
         // Variables (to track movement and where things are)
         // using geometry and cartesian plane (x,y)
-                
+
+        // Variable to track movement
+        int pacmanDirection = STOPPED;
+
         // coordinates for pacman
         int pacmanTop    = 0;
         int pacmanBottom = 0;
@@ -40,31 +45,29 @@ namespace ComputerScience20S
         int pacmanWidth  = 0;
         int pacmanHeight = 0;
 
-        int pacmanDirection = 0;
+        // coordinates for ghost1
+        int ghost1Top = 0;
+        int ghost1Bottom = 0;
+        int ghost1Left = 0;
+        int ghost1Right = 0;
+        int ghost1Width = 0;
+        int ghost1Height = 0;
 
-        // coordinates for dot
-        int dotTop = 0;
-        int dotBottom = 0;
-        int dotLeft = 0;
-        int dotRight = 0;
-        int dotWidth = 0;
-        int dotHeight = 0;
+        // coordinates for wall1
+        int wall1Top = 0;
+        int wall1Bottom = 0;
+        int wall1Left = 0;
+        int wall1Right = 0;
+        int wall1Width = 0;
+        int wall1Height = 0;
 
-        // coordinates for wall
-        int wallTop = 0;
-        int wallBottom = 0;
-        int wallLeft = 0;
-        int wallRight = 0;
-        int wallWidth = 0;
-        int wallHeight = 0;
-
-        // coordinates for ghost
-        int ghostTop = 0;
-        int ghostBottom = 0;
-        int ghostLeft = 0;
-        int ghostRight = 0;
-        int ghostWidth = 0;
-        int ghostHeight = 0;
+        // coordinates for dot1
+        int dot1Top = 0;
+        int dot1Bottom = 0;
+        int dot1Left = 0;
+        int dot1Right = 0;
+        int dot1Width = 0;
+        int dot1Height = 0;
         
 
         public frmProgrammingExtras2()
@@ -79,13 +82,15 @@ namespace ComputerScience20S
             // properties window, scroll down to "KeyDown" and double
             // click to get to this code... this code runs when the user
             // presses a key
-            
-            if      (e.KeyValue == KEY_END)   this.Close();
+
+            if      (e.KeyValue == KEY_END)   Application.Exit();
             else if (e.KeyValue == KEY_DOWN)  pacmanDirection = MOVE_DOWN;
             else if (e.KeyValue == KEY_UP)    pacmanDirection = MOVE_UP;
             else if (e.KeyValue == KEY_LEFT)  pacmanDirection = MOVE_LEFT;
             else if (e.KeyValue == KEY_RIGHT) pacmanDirection = MOVE_RIGHT;
-            else                              pacmanDirection = STOPPED;
+
+            // Not using the curly brackets is an option! (no marks given
+            // or taken away for not using them)
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -97,24 +102,107 @@ namespace ComputerScience20S
             //     are: Pacman, Wall 1, Dot 1, and Ghost 1 (where are they 
             //     at this moment in time)
 
+            // Getting pacman's current location (coordinates)
+            pacmanTop    = picPacman.Top;
+            pacmanLeft   = picPacman.Left;
+            pacmanWidth  = picPacman.Width;
+            pacmanHeight = picPacman.Height;
+            // The other two coordinates, I calculate
+            pacmanBottom = pacmanTop  + pacmanHeight;
+            pacmanRight  = pacmanLeft + pacmanWidth;
+
+            // Getting ghost's current location (coordinates)
+            ghost1Top = picGhost1.Top;
+            ghost1Left = picGhost1.Left;
+            ghost1Width = picGhost1.Width;
+            ghost1Height = picGhost1.Height;
+            // The other two coordinates, I calculate
+            ghost1Bottom = ghost1Top + ghost1Height;
+            ghost1Right = ghost1Left + ghost1Width;
+
+            // Getting dot's current location (coordinates)
+            dot1Top = picDot1.Top;
+            dot1Left = picDot1.Left;
+            dot1Width = picDot1.Width;
+            dot1Height = picDot1.Height;
+            // The other two coordinates, I calculate
+            dot1Bottom = dot1Top + dot1Height;
+            dot1Right = dot1Left + dot1Width;
+
+            // Getting wall's current location (coordinates)
+            wall1Top = picWall1.Top;
+            wall1Left = picWall1.Left;
+            wall1Width = picWall1.Width;
+            wall1Height = picWall1.Height;
+            // The other two coordinates, I calculate
+            wall1Bottom = wall1Top + wall1Height;
+            wall1Right = wall1Left + wall1Width;        
+                
             // (2) Move some of the coordinates (Pacman, potentially the
             //     ghost) - based on the direction the user set when the
             //     user preseed down a key (e.g. we move the pacman 
             //     coordinates up when the user presses the up key)
 
+            if      (pacmanDirection == MOVE_UP)    pacmanTop  = pacmanTop  - MOVE_AMOUNT;
+            else if (pacmanDirection == MOVE_DOWN)  pacmanTop  = pacmanTop  + MOVE_AMOUNT;
+            else if (pacmanDirection == MOVE_LEFT)  pacmanLeft = pacmanLeft - MOVE_AMOUNT;
+            else if (pacmanDirection == MOVE_RIGHT) pacmanLeft = pacmanLeft + MOVE_AMOUNT;
+
+            // The other two coordinates, I recalculate
+            pacmanBottom = pacmanTop  + pacmanHeight;
+            pacmanRight  = pacmanLeft + pacmanWidth;
+            
             // (3) Collision detection - is pacman "touching" with:
             //     dot (collsion reaction - dot disappears), ghost
             //     (game over), wall (stop pacman at wall)
 
+            // check if pacman is touching the dot? (if so, make the dot disappear)
+            
+            if ((( pacmanLeft   >= dot1Left && pacmanLeft   <= dot1Right ) ||
+                 ( pacmanRight  >= dot1Left && pacmanRight  <= dot1Right )) &&
+                (( pacmanTop    >= dot1Top  && pacmanTop    <= dot1Bottom) ||
+                 ( pacmanBottom >= dot1Top  && pacmanBottom <= dot1Bottom)))
+            {
+                picDot1.Visible = false;
+            }
+            else if ((( dot1Left   >= pacmanLeft && dot1Left   <= pacmanRight ) ||
+                      ( dot1Right  >= pacmanLeft && dot1Right  <= pacmanRight )) &&
+                     (( dot1Top    >= pacmanTop  && dot1Top    <= pacmanBottom) ||
+                      ( dot1Bottom >= pacmanTop  && dot1Bottom <= pacmanBottom )))
+            {
+                picDot1.Visible = false;
+            }
+
+            // check if pacman is touching the ghost? (if so, end the game)
+            if (((pacmanLeft >= ghost1Left && pacmanLeft <= ghost1Right) ||
+                 (pacmanRight >= ghost1Left && pacmanRight <= ghost1Right)) &&
+                ((pacmanTop >= ghost1Top && pacmanTop <= ghost1Bottom) ||
+                 (pacmanBottom >= ghost1Top && pacmanBottom <= ghost1Bottom)))
+            {
+                Application.Exit();
+            }
+            else if (((ghost1Left >= pacmanLeft && ghost1Left <= pacmanRight) ||
+                      (ghost1Right >= pacmanLeft && ghost1Right <= pacmanRight)) &&
+                     ((ghost1Top >= pacmanTop && ghost1Top <= pacmanBottom) ||
+                      (ghost1Bottom >= pacmanTop && ghost1Bottom <= pacmanBottom)))
+            {
+                Application.Exit();
+            }
+
+            // check if pacman is touching the wall? (if so...
+            // stop pacman: depends on which direction pacman is going)
+
+
             // (4) Redraw pacman on the form in the new location
             //     from coordinates
-        
-            getTheCoordinates();
-            moveTheCoordinates();
-            checkForCollisions();
-            picPacman.Top = pacmanTop;
+
             picPacman.Left = pacmanLeft;
+            picPacman.Top = pacmanTop;
+            
         }
+
+
+
 
         private void checkForCollisions()
         {
@@ -191,46 +279,6 @@ namespace ComputerScience20S
             }
         }
         
-        private void moveTheCoordinates()
-        {
-            const int MOVE_AMOUNT = 2;
-            if      (pacmanDirection == MOVE_UP)    pacmanTop  = pacmanTop  - MOVE_AMOUNT;
-            else if (pacmanDirection == MOVE_DOWN)  pacmanTop  = pacmanTop  + MOVE_AMOUNT; 
-            else if (pacmanDirection == MOVE_LEFT)  pacmanLeft = pacmanLeft - MOVE_AMOUNT; 
-            else if (pacmanDirection == MOVE_RIGHT) pacmanLeft = pacmanLeft + MOVE_AMOUNT; 
-            pacmanBottom = pacmanTop + pacmanHeight;
-            pacmanRight = pacmanLeft + pacmanWidth;
-        }
-
-        private void getTheCoordinates()
-        {
-            pacmanTop = picPacman.Top;
-            pacmanLeft = picPacman.Left;
-            pacmanWidth = picPacman.Width;
-            pacmanHeight = picPacman.Height;
-            pacmanBottom = pacmanTop + pacmanHeight;
-            pacmanRight = pacmanLeft + pacmanWidth;
-
-            dotTop = picDot.Top;
-            dotLeft = picDot.Left;
-            dotWidth = picDot.Width;
-            dotHeight = picDot.Height;
-            dotBottom = dotTop + dotHeight;
-            dotRight = dotLeft + dotWidth;
-
-            wallTop = picWall.Top;
-            wallLeft = picWall.Left;
-            wallWidth = picWall.Width;
-            wallHeight = picWall.Height;
-            wallBottom = wallTop + wallHeight;
-            wallRight = wallLeft + wallWidth;
-
-            ghostTop = picGhost.Top;
-            ghostLeft = picGhost.Left;
-            ghostWidth = picGhost.Width;
-            ghostHeight = picGhost.Height;
-            ghostBottom = ghostTop + ghostHeight;
-            ghostRight = ghostLeft + ghostWidth;
-        }
+        
     }
 }
