83c8d5f7535f5c9672471339093644d310dfe78b
Mr. Wachs <mrLWachs@gmail.com>
2023-03-06 11:49:42 -0600

    Update frmVariablesExample1.cs

--- ComputerScience20S/frmVariablesExample1.cs
+++ ComputerScience20S/frmVariablesExample1.cs
@@ -22,17 +22,16 @@ namespace ComputerScience20S
             
             string value1 = txtInput1.Text;
             int number1 = Convert.ToInt32(value1);
-            int number2 = number1 + 10;
-            string answer1 = "Number plus ten is " + number2;
+            int number2 = number1 * 2;
+            string answer1 = "Double the number is " + number2;
             lblOutput1.Text = answer1;
-
 
             string value2 = txtInput2.Text;
             double number3 = Convert.ToDouble(value2);
             double number4 = number3 * 0.5;
             string answer2 = "Half the number is " + number4;            
             lblOutput2.Text = answer2;
-            
+
             int number5 = (int)number4;
             double number6 = (double)number5;
             
